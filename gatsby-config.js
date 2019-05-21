fc9885ffcb12036b4aae3969cebacb67f91be7c3
Greg Mathews <gregdmathews@gmail.com>
2019-05-20 20:43:00 -0700

    Updating the title and description of the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Crocflock',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Bringing you the best croc charms for people who are unbounded by convention',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
